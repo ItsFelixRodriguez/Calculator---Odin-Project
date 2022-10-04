b70f3cdc17c146672badeec31102e09653bc128c
ItsFelixRodriguez <ItsFelixRodriguez@gmail.com>
2022-10-04 15:10:32 +0900

    built skeletons of JS functions

--- Calculator.js
+++ Calculator.js
@@ -1,1 +1,30 @@
 Calculator
+// math functions to write out
+//add
+function add (a,b) {
+    return (a+b);
+}
+//subtract
+function subtract (a,b) {
+    return (a - b);
+}
+
+//multiply
+function multiply (a,b) {
+    return (a*b);
+}
+//divide
+function divide (a,b) {
+    return (a/b);
+}
+
+function operate(){
+    //some code here that takes an operator and 2 numbers
+    //and then calls one of the functions on the numbers
+}
+
+//function to capture number 'a' from 
+//inputs on screen buttons
+
+//function to capture number 'b' from 
+//inputs on screen buttons
