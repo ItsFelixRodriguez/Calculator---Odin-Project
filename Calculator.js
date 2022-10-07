de48f162f5700662294417d1f3cd1f5253f7e27d
ItsFelixRodriguez <ItsFelixRodriguez@gmail.com>
2022-10-07 14:51:40 +0900

    rebuilt HTML, CSS, JS so buttons work

--- Calculator.js
+++ Calculator.js
@@ -1,4 +1,52 @@
 //Calculator
+
+// functions to make buttons work//
+
+// Clicking an input fires the input function
+document.getElementById('0').addEventListener('click', input);
+document.getElementById('1').addEventListener('click', input);
+document.getElementById('2').addEventListener('click', input);
+document.getElementById('3').addEventListener('click', input);
+document.getElementById('4').addEventListener('click', input);
+document.getElementById('5').addEventListener('click', input);
+document.getElementById('6').addEventListener('click', input);
+document.getElementById('7').addEventListener('click', input);
+document.getElementById('8').addEventListener('click', input);
+document.getElementById('9').addEventListener('click', input);
+document.getElementById('+').addEventListener('click', input);
+document.getElementById('-').addEventListener('click', input);
+document.getElementById('*').addEventListener('click', input);
+document.getElementById('/').addEventListener('click', input);
+document.getElementById('.').addEventListener('click', input);
+
+
+//Equals fires the calculate function
+document.getElementById('=').addEventListener('click', operate);
+
+//AC fires the reset function
+document.getElementById('AC').addEventListener('click', reset);
+
+const display = document.getElementById('display-text');
+let displayCurrent = '';
+
+function input(e) {
+let inputValue = e.target.innerText;
+displayCurrent += inputValue;
+display.innerText = displayCurrent;
+}
+function operate() {
+display.innerTExt = eval(displayCurrent);
+displayCurrent = eval(displayCurrent).toString();
+}
+
+function reset () {
+displayCurrent = " ";
+display.innerText = displayCurrent;
+}
+
+
+
+
 // math functions to write out
 //add
 function add (a,b) {
@@ -23,9 +71,7 @@ function operate(){
     //and then calls one of the functions on the numbers
 }
 
-calc = [
-    {name: }
-]
+
 //function to capture number 'a' from 
 //inputs on screen buttons
 
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Calculator---Odin-Project
 
 Okay this will be the big one. I will stick with this one as long as it takes to make it work. 
+
+10/5 -- Successfully built the CSS Grid for the calculator buttons. 
+Things to do still:
+- Tweak the colors to make the design look more in line with what I am looking for.
+- Get the numbers to display upon click event.
