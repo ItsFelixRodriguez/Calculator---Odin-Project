4ecfc48cda3203b6751def237fd7c1677dbfc35e
ItsFelixRodriguez <ItsFelixRodriguez@gmail.com>
2022-10-11 16:21:54 +0900

    Commit

--- Calculator.js
+++ Calculator.js
@@ -66,10 +66,7 @@ function divide (a,b) {
     return (a/b);
 }
 
-function operate(){
-    //some code here that takes an operator and 2 numbers
-    //and then calls one of the functions on the numbers
-}
+
 
 
 //function to capture number 'a' from 
